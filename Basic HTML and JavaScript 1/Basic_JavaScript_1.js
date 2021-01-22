13d503bf97d62eb05a3df185dfec2d98745450f2
Liam Curtin <77279572+Snowman86ie@users.noreply.github.com>
2021-01-22 15:01:00 +0000

    basic HMTL and JavaScript
    A simple webpage displaying the uses of the alt attribute in a imgae and the uses of different style fonts, ie size, color, text style.

---
+++ Basic HTML and JavaScript 1/Basic_JavaScript_1.js
@@ -0,0 +1,7 @@
+alert("Hello World, I'm an alert box using JavaScript!");
+
+function My_First_Function() {
+		var String = "Kiss me, I'm Irish!";
+		var result = String.fontcolor("green");
+		document.getElementById("Irish").innerHTML = result;
+		}
